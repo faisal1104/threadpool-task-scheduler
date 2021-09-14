0f09c87a9115bd8e6d7251c5bd21d51227fabf73
bs23.faisal <faisal.chowdhury@brainstation23.com>
2021-09-14 23:42:33 +0600

    block multiple scheduler for a single instance

--- src/main/java/com/example/threadpooltaskschedulerdemo/controller/Sheduler1Controller.java
+++ src/main/java/com/example/threadpooltaskschedulerdemo/controller/Sheduler1Controller.java
@@ -10,21 +10,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/scheduler1")
 public class Sheduler1Controller {
 
-  @Autowired
-  private ThreadPoolTaskScheduler taskScheduler;
+  @Autowired private ThreadPoolTaskScheduler taskScheduler;
 
   @PostMapping("/with-cron-expression")
-  public String startCronScheduler(@RequestParam String cronExpression){
+  public String startCronScheduler(@RequestParam String cronExpression) {
     var cronTrigger = new CronTrigger(cronExpression);
+
+    var executor = taskScheduler.getScheduledThreadPoolExecutor();
+
+    System.out.println(taskScheduler.getThreadNamePrefix());
+
+    //boolen isDaemon is used to block multiple scheduler of a single instance
+    if (taskScheduler.isDaemon()) return "Scheduler-1 is already running";
+
     taskScheduler.setBeanName("scheduler1");
     taskScheduler.initialize();
+    taskScheduler.setDaemon(true);
     taskScheduler.schedule(new ThreadPoolTaskSchedulerDemo1(), cronTrigger);
     return "Scheduler-1 with cron expression started";
   }
 
   @GetMapping("/stop")
-  public String stopScheduler(){
+  public String stopScheduler() {
+    if (!taskScheduler.isDaemon()) return "Scheduler-1 is not started yet";
     taskScheduler.shutdown();
-    return "Scheduler-2 Stopped";
+    return "Scheduler-1 Stopped";
   }
 }
--- src/main/java/com/example/threadpooltaskschedulerdemo/controller/Sheduler2Controller.java
+++ src/main/java/com/example/threadpooltaskschedulerdemo/controller/Sheduler2Controller.java
@@ -10,20 +10,26 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/scheduler2")
 public class Sheduler2Controller {
 
-  @Autowired
-  private ThreadPoolTaskScheduler taskScheduler;
+  @Autowired private ThreadPoolTaskScheduler taskScheduler;
 
   @PostMapping("/with-cron-expression")
-  public String startCronScheduler(@RequestParam String cronExpression){
+  public String startCronScheduler(@RequestParam String cronExpression) {
     var cronTrigger = new CronTrigger(cronExpression);
+
+    // boolen isDaemon is used to block multiple scheduler of a single instance
+    if (taskScheduler.isDaemon()) return "Scheduler-2 is already running.";
+
     taskScheduler.setBeanName("scheduler2");
     taskScheduler.initialize();
+    taskScheduler.setDaemon(true);
     taskScheduler.schedule(new ThreadPoolTaskSchedulerDemo2(), cronTrigger);
+    System.out.println(taskScheduler.isDaemon());
     return "Scheduler-2 with cron expression started";
   }
 
   @GetMapping("/stop")
-  public String stopScheduler(){
+  public String stopScheduler() {
+    if (!taskScheduler.isDaemon()) return "Scheduler-2 is not started yet";
     taskScheduler.shutdown();
     return "Scheduler-2 stopped";
   }
